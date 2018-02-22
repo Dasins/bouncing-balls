89c6ec6e51245d584afc512d6db6ab33cfd6b911
Dasins <dm.evilart@gmail.com>
2018-02-23 00:41:57 +0100

    Crea cualquier numero de bolas, de diametro aleatorio que botan hasta que una alcanza el final de la linea

--- BallDemo.java
+++ BallDemo.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.util.Random;
+import java.util.ArrayList;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -49,5 +51,49 @@ public class BallDemo
                 finished = true;
             }
         }
+    }
+    
+    /**
+     * Simula un numero indicado por el usuario de bolas saltarinas.
+     */
+    public void bounce(int numBolas) {
+        // Posicion de la linea del suelo
+        int suelo = 400;
+        
+        myCanvas.setVisible(true);
+        myCanvas.drawLine(50,suelo,550,suelo);
+        
+        ArrayList<BouncingBall> bolas = new ArrayList<>();
+        // Crea tantas bolas aleatorias como se indique por parametro.
+        for(int i = 0; i < numBolas; i++){  
+            bolas.add(bolaAleatoria(suelo));
+        }
+        
+        boolean finalizado =  false;
+        while(!finalizado) {
+            // Pequeno retraso.
+            myCanvas.wait(50);
+            for(BouncingBall bola : bolas) {
+                bola.move();
+            }
+            int i = 0;
+            // Detiene la bola tras moverse lo suficiente en el eje x.
+            while(!finalizado && i < bolas.size()) {
+                if(bolas.get(i).getXPosition() >= 550) {
+                    finalizado = true;
+                }
+                i++;
+            }
+        }
+    }
+    
+    /**
+     * Devuelve una bola aleatoria.
+     * @return Devuelve una bola aleatoria.
+     */
+    private BouncingBall bolaAleatoria(int suelo) {
+        Random aleatorio = new Random();
+        int diametro = aleatorio.nextInt(41) + 10;
+        return new BouncingBall(50, 50, diametro, Color.BLUE, suelo, myCanvas);
     }
 }
