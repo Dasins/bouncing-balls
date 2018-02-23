5e83e99b7a5bd238b2785b42007ec2a921545612
Dasins <dm.evilart@gmail.com>
2018-02-23 01:32:05 +0100

    Las bolas aparece en una posicion aleatoria, en el cuadrante superior izquierdo del lienzo

--- BallDemo.java
+++ BallDemo.java
@@ -54,21 +54,45 @@ public class BallDemo
     }
     
     /**
-     * Simula un numero indicado por el usuario de bolas saltarinas.
+     * Simula tantas bolas saltarinas aleatorias como se indica por parametro.
+     * @param numBolas El numero de bolas aleatorias que se quieren hacer botar. 
      */
     public void bounce(int numBolas) {
         // Posicion de la linea del suelo
         int suelo = 400;
+        int limiteDerecho = 550; // La ultima posicion de suelo a la derecha.
         
         myCanvas.setVisible(true);
-        myCanvas.drawLine(50,suelo,550,suelo);
+        myCanvas.drawLine(50,suelo,limiteDerecho,suelo);
         
         ArrayList<BouncingBall> bolas = new ArrayList<>();
         // Crea tantas bolas aleatorias como se indique por parametro.
         for(int i = 0; i < numBolas; i++){  
             bolas.add(bolaAleatoria(suelo));
-        }
-        
+            bolas.get(i).draw();
+        }  
+        botar(bolas,limiteDerecho);
+    }
+    
+    /**
+     * Devuelve una bola aleatoria, las bolas aleatorias tienen un color, una posicion inicial y un diametro aleatorio.
+     * @param suelo La posicion del suelo, donde la bola va a rebotar.
+     * @return Devuelve una bola aleatoria.
+     */
+    private BouncingBall bolaAleatoria(int suelo) {
+        Random aleatorio = new Random();
+        int xPos = aleatorio.nextInt(126) + 25;
+        int yPos = aleatorio.nextInt(76);
+        int diametro = aleatorio.nextInt(31) + 5;
+        Color color = new Color(aleatorio.nextInt(256),aleatorio.nextInt(256),aleatorio.nextInt(256));
+        return new BouncingBall(xPos, yPos, diametro, color, suelo, myCanvas);
+    }
+    
+    /**
+     * Hace que todas las bolas de la coleccion indicada por parametro se pongan a botar, hasta que una se salga del suelo por la derecha.
+     * @param bolas La coleccion de bolas que se quiere hacer rebotar.
+     */
+    private void botar(ArrayList<BouncingBall> bolas, int limiteDerecho) {
         boolean finalizado =  false;
         while(!finalizado) {
             // Pequeno retraso.
@@ -79,7 +103,7 @@ public class BallDemo
             int i = 0;
             // Detiene la bola tras moverse lo suficiente en el eje x.
             while(!finalizado && i < bolas.size()) {
-                if(bolas.get(i).getXPosition() >= 550) {
+                if(bolas.get(i).getXPosition() >= limiteDerecho) {
                     finalizado = true;
                 }
                 i++;
@@ -87,14 +111,5 @@ public class BallDemo
         }
     }
     
-    /**
-     * Devuelve una bola aleatoria.
-     * @return Devuelve una bola aleatoria.
-     */
-    private BouncingBall bolaAleatoria(int suelo) {
-        Random aleatorio = new Random();
-        int diametro = aleatorio.nextInt(41) + 10;
-        Color color = new Color(aleatorio.nextInt(256),aleatorio.nextInt(256),aleatorio.nextInt(256));
-        return new BouncingBall(50, 50, diametro, color, suelo, myCanvas);
-    }
+    
 }
